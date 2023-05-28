c8029c23d5a39a6029b5313fc3af89d12fddb6b0
mafefreeitas <128387890+mafefreeitas@users.noreply.github.com>
2023-05-28 19:15:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 """Main functions"""
 
-
+import re
 def is_palindrome(string: str) -> bool:
     """Check if string is palindrome."""
+    string=string.lower().replace(" ", "")
+    string=re.sub(r'[^\w\s]','',string)
+    string_inversa= string[::-1]
+    
+    if string == string_inversa:
+        return True
+    else:
     return False
